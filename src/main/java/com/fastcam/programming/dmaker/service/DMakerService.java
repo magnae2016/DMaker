4aa831f047ebb445eee1fda49c0775b351541c32
magnae2016 <magnae2016@gmail.com>
2022-08-22 22:29:10 +0900

    Add business validation logic

--- src/main/java/com/fastcam/programming/dmaker/service/DMakerService.java
+++ src/main/java/com/fastcam/programming/dmaker/service/DMakerService.java
@@ -2,12 +2,20 @@ package com.fastcam.programming.dmaker.service;
 
 import com.fastcam.programming.dmaker.dto.CreateDeveloper;
 import com.fastcam.programming.dmaker.entity.Developer;
+import com.fastcam.programming.dmaker.exception.DMakerErrorCode;
+import com.fastcam.programming.dmaker.exception.DMakerException;
 import com.fastcam.programming.dmaker.repository.DeveloperRepository;
 import com.fastcam.programming.dmaker.type.DeveloperLevel;
 import com.fastcam.programming.dmaker.type.DeveloperSkillType;
+import jdk.nashorn.internal.runtime.options.Option;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static com.fastcam.programming.dmaker.exception.DMakerErrorCode.DUPLICATED_MEMBER_ID;
+import static com.fastcam.programming.dmaker.exception.DMakerErrorCode.LEVEL_EXPERIENCE_YEARS_NOT_MATCHED;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +24,8 @@ public class DMakerService {
 
     @Transactional
     public void createDeveloper(CreateDeveloper.Request request) {
+        validateCreateDeveloperRequest(request);
+
         Developer developer = Developer.builder()
                 .developerLevel(DeveloperLevel.SENIOR)
                 .developerSkillType(DeveloperSkillType.FRONT_END)
@@ -25,5 +35,29 @@ public class DMakerService {
                 .build();
         developerRepository.save(developer);
 
+    }
+
+    private void validateCreateDeveloperRequest(CreateDeveloper.Request request) {
+        DeveloperLevel developerLevel = request.getDeveloperLevel();
+        Integer experienceYears = request.getExperienceYears();
+        if (developerLevel == DeveloperLevel.SENIOR
+                && experienceYears < 10) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+        if (developerLevel == DeveloperLevel.JUNGNIOR
+        && (experienceYears < 4 || experienceYears > 10)) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+        if (developerLevel == DeveloperLevel.JUNIOR && experienceYears > 4) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+
+//        Optional<Developer> developer = developerRepository.findByMemberId(request.getMemberId());
+//        if (developer.isPresent())
+//            throw new DMakerException(DUPLICATED_MEMBER_ID)
+
+        developerRepository.findByMemberId(request.getMemberId()).ifPresent(developer -> {
+            throw new DMakerException(DUPLICATED_MEMBER_ID);
+        });
     }
 }
